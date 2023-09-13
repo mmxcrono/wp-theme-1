e8f9c84339719d25ca25e1f1a5ffa2b4958e9208
Hoang <hnguyen.email@gmail.com>
2023-09-12 20:11:06 -0700

    feat: Add initial files

---
+++ plugins/hello_wp.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Plugin Name: Hello wp
+  Plugin URI: http://wp.org/plugins/hello-wp/
+  Description: Hello wp!
+  Author: Hoang Nguyen
+  Version: 1.0.0
+  Author URI: https://mmxcrono.github.io
+*/
