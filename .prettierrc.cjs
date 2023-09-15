a205daf52a5bd62bfb9d00fd16b7f61e15c561bd
Hoang <hnguyen.email@gmail.com>
2023-09-14 17:27:01 -0700

    chore: Clean up prettier. feat: clean up excess margin and padding

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,10 +1,3 @@
 module.exports = {
   "plugins": ["@prettier/plugin-php"],
-
-  "trailingComma": "all",
-  "tabWidth": 2,
-  "semi": true,
-  "singleQuote": true,
-  "printWidth": 80,
-  "bracketSpacing": true,
 }
--- themes/hello/patterns/hidden-comments.php
+++ themes/hello/patterns/hidden-comments.php
@@ -6,11 +6,15 @@
  */
 ?>
 <!-- wp:group {"layout":{"type":"constrained"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+<div class="wp-block-group" >
 	<!-- wp:comments -->
 	<div class="wp-block-comments">
 		<!-- wp:heading {"level":2} -->
-		<h2><?php echo esc_html_x( 'Comments', 'Title of comments section', 'twentytwentythree' ); ?></h2>
+		<h2><?php echo esc_html_x(
+      "Comments",
+      "Title of comments section",
+      "twentytwentythree"
+  ); ?></h2>
 		<!-- /wp:heading -->
 
 		<!-- wp:comments-title {"level":3} /-->
